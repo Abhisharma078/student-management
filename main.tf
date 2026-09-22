672e13a695a34a20454b3b2d5b21bf832fc2315a
Abhi Sharma <abhimudgal2k.sharma@gmail.com>
2026-09-23 00:33:56 +0530

    update S3 backend region in Terraform configuration from ap-south-1 to us-west-1

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ terraform {
   backend "s3" {
     bucket       = "abhicloud.online"
     key          = "student-management/eks/terraform.tfstate"
-    region       = "ap-south-1"
+    region       = "us-west-1"
     use_lockfile = true
   }
 }
